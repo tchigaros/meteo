20997bc72b2b26f4af8dea72397f265bace52ab0
tchigaros <49082397+tchigaros@users.noreply.github.com>
2019-12-15 18:07:37 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -157,7 +157,7 @@ class App extends React.Component {
           pressure: data3.main.pressure,
           description: data3.weather[0].description,
           iconcode:data4.list[0].weather[0].icon,
-          iconurl:"http://openweathermap.org/img/w/" + ${iconcode} + ".png",
+          iconurl:"https://openweathermap.org/img/w/" + iconcode + ".png",
           error: "",
       });
 
@@ -177,7 +177,7 @@ class App extends React.Component {
           pressure: data3.main.pressure,
           description: data3.weather[0].description,
           iconcode:data4.list[0].weather[0].icon,
-          iconurl:"http://openweathermap.org/img/w/" + ${iconcode} + ".png",
+          iconurl:"https://openweathermap.org/img/w/" + iconcode + ".png",
           error: "",
       });
 
