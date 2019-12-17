33be608ac9fec2852e7236f4ab0a399f0f40eb82
tchigaros <49082397+tchigaros@users.noreply.github.com>
2019-12-17 01:05:38 +0100

    Update new_Weather.js

--- src/components/new_Weather.js
+++ src/components/new_Weather.js
@@ -30,7 +30,7 @@ const Weather = props => (
 	     props.date && <th className="weather__key" >{ props.date }</th>
 	    }
 	     {
-	     props.description && <th className="weather__key" >{ props.description } <img src={props.iconurl} className="icondescription"></img></th>
+	     props.description && <th className="weather__key" >{ props.description + props.description } <img src={props.iconurl} className="icondescription"></img></th>
 	    }
 
 	    
