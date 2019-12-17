673827ed31367d6991a812399159160e6b1c24ba
tchigaros <49082397+tchigaros@users.noreply.github.com>
2019-12-17 21:54:03 +0100

    Update new_Weather.js

--- src/components/new_Weather.js
+++ src/components/new_Weather.js
@@ -57,7 +57,7 @@ const Weather = props => (
 	     props.date1 && <th className="weather__key" >{ props.date1 }</th>
 	    }
 	     {
-	     props.description1 && <th className="weather__key" >{ props.description1 } <img src={"https://openweathermap.org/img/w/" + props.iconcode1 + ".png"} className="icondescription"></img></th>
+	     props.description1 && <th className="weather__key" >{ props.description1 } <img src={"https://www.weatherbit.io/static/img/icons/" + props.iconcode1 + ".png"} className="icondescription"></img></th>
 	    }
 
 	    
@@ -82,7 +82,7 @@ const Weather = props => (
 	     props.date2 && <th className="weather__key" >{ props.date2 }</th>
 	    }
 	     {
-	     props.description2 && <th className="weather__key" >{ props.description2 } <img src={"https://openweathermap.org/img/w/" + props.iconcode2 + ".png"} className="icondescription"></img></th>
+	     props.description2 && <th className="weather__key" >{ props.description2 } <img src={"https://www.weatherbit.io/static/img/icons/" + props.iconcode2 + ".png"} className="icondescription"></img></th>
 	    }
 
 	    
