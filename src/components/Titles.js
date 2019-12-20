5fdc2704d3e15476d59e756837a6d0f1ab3bb4e5
tchigaros <49082397+tchigaros@users.noreply.github.com>
2019-12-20 19:23:24 +0100

    Update Titles.js

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -7,7 +7,7 @@ const Titles = () => (
 		<p><img src={profile} size="(max-width: 600px) 100vw, 600px" className="profile_image"></img></p>
 		<p><img src={icon} alt="icon" size="(max-width: 600px) 100vw, 600px" className="main_icon"></img></p>
 		<p><h1 className="title-container__title">Global Weather </h1>
-		<h3 className="title-container__subtitle">The weather now ,tomorrow and more ...</h3>
+		<h3 className="title-container__subtitle">Make it Simple</h3>
 		<h4 >Abderrahmane CHIGAR</h4></p>
 		
 	</div>
